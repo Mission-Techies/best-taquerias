dadc55fd8cec178c5e729329f6b0955c5fd6d472
smcueto <stephaniem.cueto@gmail.com>
2016-12-06 19:09:04 +0000

    Added Some Pages. Need Help with CSS on main page

--- app.js
+++ app.js
@@ -1,26 +1,2 @@
-var api_key = 'keyjUyqxvapBphwo4';
-var restaurant_record = 'https://api.airtable.com/v0/appSrgke7E0ElZhMY/Restaurants?api_key=' + api_key;
-
-function renderRecords(data) {
-  $(data.records).each(function(index, taquerias) {
-    var taquerias_name = taquerias.fields['Name'];
-    var taquerias_where = taquerias.fields['District'];
-    var taquerias_pics = taquerias.fields['Pictures'];
-    var taquerias_info = '';
-    if (taquerias_name) {
-      taquerias_info += `<li>`;
-      if (taquerias_pics) {
-        $.each(taquerias_pics, function(i, pic) {
-          taquerias_info += `<img src="${pic.url}">`;
-        });
-      }
-      taquerias_info += ` Name: ${taquerias_name} <br> Where: ${taquerias_where}`;
-      taquerias_info += `</li>`;
-    }
-    $('.taquerias').append(taquerias_info);
-  });
-}
-
-$.get(restaurant_record, renderRecords);
 
 
