fe4062fba1d1a1abeda5fcd5c2216f9672192578
smcueto <stephaniem.cueto@gmail.com>
2016-12-14 19:13:13 +0000

    trying to add about

--- abouttaqueriasf.js
+++ abouttaqueriasf.js
@@ -5,21 +5,21 @@ var about_record = 'https://api.airtable.com/v0/appSrgke7E0ElZhMY/About?api_key=
 function renderRecords(data) {
   $(data.records).each(function(index, amakers) {
     var amakers_name = amakers.fields['Name'];
-    var amakers_pics = amakers.fields['Pictures'];
-    var amakers_des = amakers.fields['Description']
+    var amakers_picture = amakers.fields['Pictures'];
+    var amakers_description = amakers.fields['Description']
     var amakers_projects = amakers.fields['Projects']
     var amakers_info = '';
     if (amakers_name) {
       amakers_info += `<li>`;
-      if (amakers_pics) {
-        $.each(amakers_pics, function(i, pic) {
+      if (amakers_picture) {
+        $.each(amakers_picture, function(i, pic) {
           amakers_info += `<img src="${pic.url}">`;
         });
       }
-      amakers_info += `<br> Name: ${amakers_name} <br> About: ${amakers_des}<br> Future Projects: ${amakers_projects} <br>`;
+      amakers_info += `<br> Name: ${amakers_name} <br> About: ${amakers_description}<br> Future Projects: ${amakers_projects} <br>`;
       amakers_info += `</li>`;
     }
-    $('.about').append(amakers_info);
+    $('.amakers').append(amakers_info);
   });
 }
 
